e7ec46aeb86d5050b84537ab749ce3b634eae71f
ywalakamar <barxwells@gmail.com>
2023-04-02 01:08:39 +0300

    feat(component):add footer component

---
+++ src/components/Footer.js
@@ -0,0 +1,128 @@
+import styled from "styled-components";
+import FacebookOutlinedIcon from "@mui/icons-material/FacebookOutlined";
+import TwitterIcon from "@mui/icons-material/Twitter";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import PinterestIcon from "@mui/icons-material/Pinterest";
+import PhoneIcon from "@mui/icons-material/Phone";
+import PlaceIcon from "@mui/icons-material/Place";
+import EmailIcon from "@mui/icons-material/Email";
+
+const Container = styled.div`
+  display: flex;
+`;
+const Left = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+`;
+const Logo = styled.h1``;
+const Desc = styled.p`
+  margin: 20px 0;
+`;
+const SocialContainer = styled.div`
+  display: flex;
+`;
+const SocialIcon = styled.div`
+  width: 40px;
+  height: 40px;
+  border-radius: 50%;
+  color: white;
+  background-color: #${(props) => props.color};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin-right: 20px;
+`;
+const Center = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Title = styled.h3`
+  margin-bottom: 30px;
+`;
+const List = styled.ul`
+  margin: 0;
+  padding: 0;
+  list-style: none;
+  display: flex;
+  flex-wrap: wrap;
+`;
+const ListItem = styled.li`
+  width: 50%;
+  margin-bottom: 10px;
+`;
+const ContactItem = styled.div`
+  margin-bottom: 10px;
+  display: flex;
+  align-items: center;
+`;
+const Payment = styled.img`
+  width: 50%;
+`;
+
+const Right = styled.div`
+  flex: 1;
+  padding: 20px;
+`;
+
+const Footer = () => {
+  return (
+    <Container>
+      <Left>
+        <Logo>Orderfy</Logo>
+        <Desc>
+          Have you ever wondered how you can improve your Git commit messages?
+          This guide outlines steps to elevate your commit messages that you can
+          start implementing today.
+        </Desc>
+        <SocialContainer>
+          <SocialIcon color="385999">
+            <FacebookOutlinedIcon />
+          </SocialIcon>
+          <SocialIcon color="E4405F">
+            <InstagramIcon />
+          </SocialIcon>
+          <SocialIcon color="55ACEE">
+            <TwitterIcon />
+          </SocialIcon>
+          <SocialIcon color="E60023">
+            <PinterestIcon />
+          </SocialIcon>
+        </SocialContainer>
+      </Left>
+      <Center>
+        <Title>Useful Links</Title>
+        <List>
+          <ListItem>Home</ListItem>
+          <ListItem>Cart</ListItem>
+          <ListItem>My Account</ListItem>
+          <ListItem>Men Category</ListItem>
+          <ListItem>Female Category</ListItem>
+          <ListItem>Order Tracking</ListItem>
+          <ListItem>WishListItem</ListItem>
+          <ListItem>Terms & Consitions</ListItem>
+        </List>
+      </Center>
+      <Right>
+        <Title>Contact</Title>
+        <ContactItem>
+          <PlaceIcon style={{ marginRight: "10px" }} />
+          Kenyatta Avenue, Nairobi, Kenya
+        </ContactItem>
+        <ContactItem>
+          <PhoneIcon style={{ marginRight: "10px" }} />
+          +254 (0) 708 344 488
+        </ContactItem>
+        <ContactItem>
+          <EmailIcon style={{ marginRight: "10px" }} />
+          contat@orderfy.dev
+        </ContactItem>
+        <Payment src="https://i.ibb.co/Qfvn4z6/payment.png" />
+      </Right>
+    </Container>
+  );
+};
+
+export default Footer;
