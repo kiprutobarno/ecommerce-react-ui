3f3a46d5291864c2b5462fd3190ea7165a6b4b8f
ywalakamar <barxwells@gmail.com>
2023-04-01 17:29:07 +0300

    feat(navbar):create app navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,87 @@
+import React from "react";
+import styled from "styled-components";
+import SearchIcon from "@mui/icons-material/Search";
+import Badge from "@mui/material/Badge";
+import ShoppingCartOutlinedIcon from "@mui/icons-material/ShoppingCartOutlined";
+
+const Container = styled.div`
+  height: 60x;
+`;
+
+const Wrapper = styled.div`
+  padding: 10px 20px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Left = styled.div`
+  display: flex;
+  flex: 1;
+  align-items: center;
+`;
+const Centre = styled.div`
+  flex: 1;
+  text-align: center;
+`;
+const Logo = styled.h1`
+  font-weight: bold;
+`;
+const Right = styled.div`
+  flex: 1;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+`;
+
+const Language = styled.span`
+  font-size: 16px;
+  cursor: pointer;
+`;
+
+const SearchContainer = styled.div`
+  border: 0.5px solid lightgreen;
+  display: flex;
+  align-items: center;
+  margin-left: 25px;
+  padding: 5px;
+`;
+
+const Input = styled.input`
+  border: none;
+`;
+
+const MenuItem = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  margin-left: 25px;
+`;
+
+const Navbar = () => {
+  return (
+    <Container>
+      <Wrapper>
+        <Left>
+          <Language>EN</Language>
+          <SearchContainer>
+            <Input />
+            <SearchIcon style={{ color: "gray", fontSize: 16 }}></SearchIcon>
+          </SearchContainer>
+        </Left>
+        <Centre>
+          <Logo>Orderfy</Logo>
+        </Centre>
+        <Right>
+          <MenuItem>Register</MenuItem>
+          <MenuItem>Sign In</MenuItem>
+          <MenuItem>
+            <Badge badgeContent={4} color="primary">
+              <ShoppingCartOutlinedIcon />
+            </Badge>
+          </MenuItem>
+        </Right>
+      </Wrapper>
+    </Container>
+  );
+};
+
+export default Navbar;
