df6777d5997a74dfce95f3266ac77b1990b218eb
ywalakamar <barxwells@gmail.com>
2023-04-05 12:46:59 +0300

    feat(router):add page navigation using router

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
-// import Home from "./pages/Home";
-// import Product from "./pages/Product";
-// import Cart from "./pages/Cart";
-// import Login from "./pages/Login";
-// import Register from "./pages/Register";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+import Cart from "./pages/Cart";
+import Login from "./pages/Login";
+import Register from "./pages/Register";
 import ProductList from "./pages/productList";
 
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 const App = () => {
+  const user = true;
   return (
-    <div className="App">
-      {/*  <Home /> */}
-      <ProductList />
-      {/* <Product /> */}
-      {/* <Register /> */}
-      {/* <Login /> */}
-      {/* <Cart/> */}
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/products" element={<ProductList />} />
+        <Route path="/products/:id" element={<Product />} />
+        <Route path="/cart" element={<Cart />} />
+        <Route
+          path="/register"
+          element={user ? <Navigate to="/" /> : <Register />}
+        />
+        <Route
+          path="/login"
+          element={user ? <Navigate to="/" /> : <Login />}
+        ></Route>
+      </Routes>
+    </Router>
   );
 };
 
--- src/components/CategoryItem.js
+++ src/components/CategoryItem.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { mobile } from "../responsive";
+import { Link } from "react-router-dom";
 
 const Container = styled.div`
   flex: 1;
@@ -47,11 +48,13 @@ const Button = styled.button`
 const CategoryItem = ({ item }) => {
   return (
     <Container>
-      <Image src={item.img}></Image>
-      <Info>
-        <Title>{item.title}</Title>
-        <Button>Shop Now</Button>
-      </Info>
+      <Link to={`/products/${item.category}`}>
+        <Image src={item.img}></Image>
+        <Info>
+          <Title>{item.title}</Title>
+          <Button>Shop Now</Button>
+        </Info>
+      </Link>
     </Container>
   );
 };
